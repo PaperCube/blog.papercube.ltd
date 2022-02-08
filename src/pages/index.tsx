c449296edf73a0abca9c3c79c4a493565cc0f590
PaperCube <imzhy@hotmail.com>
2022-02-08 20:27:11 +0800

    Change links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Read Documentations
+            to="/blog">
+            Read My Blogs
           </Link>
         </div>
       </div>
